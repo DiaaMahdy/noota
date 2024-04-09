ec997a536198b4bd6bfd8161a513eb83c7d2e4b2
DiaaMAhdy <diaaalmahdy77@gmail.com>
2024-04-09 22:52:02 +0200

    add note cubit

---
+++ lib/cubits/notes_cubit.dart
@@ -0,0 +1,8 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+
+part 'notes_state.dart';
+
+class NotesCubit extends Cubit<AddNoteState> {
+  NotesCubit(super.initialState);
+}
